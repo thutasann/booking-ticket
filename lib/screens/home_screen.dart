7600017aa49911eb99cea996a29b64aa7f9e5fda
thutasann <thutasann2002@gmail.com>
2023-03-01 12:39:56 +0630

    UI: Welcome Banner

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,12 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Color(0xFFeeedf2),
+    return Scaffold(
+      backgroundColor: const Color(0xFFeeedf2),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        children: [
+          Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                      Text("Good Morning"),
+                      Text("Book Tickets"),
+                    ],
+                  ),
+                  Container(
+                    height: 60,
+                    width: 60,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image: const DecorationImage(
+                          fit: BoxFit.fitHeight,
+                          image: AssetImage("images/logo.png"),
+                        )),
+                  )
+                ],
+              )
+            ],
+          )
+        ],
+      ),
     );
   }
 }
