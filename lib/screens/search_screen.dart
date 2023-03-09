119354f4610902cc807ddf6bf21ebfb4d69715a6
thutasann <thutasann2002@gmail.com>
2023-03-09 13:26:25 +0630

    UI: Search Tab Upcoming Flights

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -123,6 +123,160 @@ class SearchScreen extends StatelessWidget {
               bigText: "Upcoming Flights",
               smallText: "View all",
             ),
+
+            // GRID SECTION
+            Gap(AppLayout.getHeight(15)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                // FRIST COLUMN
+                Container(
+                  height: AppLayout.getHeight(425),
+                  width: size.width * 0.42,
+                  padding: EdgeInsets.symmetric(
+                    horizontal: AppLayout.getHeight(15),
+                    vertical: AppLayout.getWidth(15),
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(
+                      AppLayout.getHeight(20),
+                    ),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.shade200,
+                        blurRadius: 1,
+                        spreadRadius: 1,
+                      )
+                    ],
+                  ),
+                  child: Column(children: [
+                    Container(
+                      height: AppLayout.getHeight(190),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(
+                          AppLayout.getHeight(12),
+                        ),
+                        image: const DecorationImage(
+                          fit: BoxFit.cover,
+                          image: AssetImage("images/airplane_inside.jpeg"),
+                        ),
+                      ),
+                    ),
+                    Gap(AppLayout.getHeight(12)),
+                    Text(
+                      "20% discount on the early booking for this flight, Don't miss.",
+                      style: Styles.headLineStyle2,
+                    ),
+                  ]),
+                ),
+
+                // SECOND COLUMN
+                Column(
+                  children: [
+                    // FIRST ROW
+                    Stack(
+                      children: [
+                        Container(
+                          width: size.width * 0.44,
+                          height: AppLayout.getHeight(200),
+                          decoration: BoxDecoration(
+                            color: const Color(0xFF3AB8B8),
+                            borderRadius: BorderRadius.circular(
+                              AppLayout.getHeight(18),
+                            ),
+                          ),
+                          padding: EdgeInsets.symmetric(
+                            vertical: AppLayout.getHeight(15),
+                            horizontal: AppLayout.getWidth(15),
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Discount\nfor survey",
+                                style: Styles.headLineStyle2.copyWith(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                ),
+                              ),
+                              Gap(AppLayout.getHeight(10)),
+                              Text(
+                                "Take the survey about your services and get discount",
+                                style: Styles.headLineStyle2.copyWith(
+                                  fontWeight: FontWeight.w500,
+                                  color: Colors.white,
+                                  fontSize: 18,
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                        Positioned(
+                          right: -45,
+                          top: -40,
+                          child: Container(
+                            padding: EdgeInsets.all(AppLayout.getHeight(30)),
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              border: Border.all(
+                                width: 18,
+                                color: const Color(0xFF189999),
+                              ),
+                              color: Colors.transparent,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                    Gap(AppLayout.getHeight(15)),
+
+                    // SECOND ROW
+                    Container(
+                      width: size.width * 0.44,
+                      height: AppLayout.getHeight(210),
+                      padding: EdgeInsets.symmetric(
+                        vertical: AppLayout.getHeight(15),
+                        horizontal: AppLayout.getHeight(15),
+                      ),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(
+                          AppLayout.getHeight(18),
+                        ),
+                        color: const Color(0xFFEC6545),
+                      ),
+                      child: Column(children: [
+                        Text(
+                          "Take love",
+                          style: Styles.headLineStyle2.copyWith(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                        Gap(AppLayout.getHeight(5)),
+                        RichText(
+                          text: const TextSpan(children: [
+                            TextSpan(
+                              text: "😻",
+                              style: TextStyle(fontSize: 38),
+                            ),
+                            TextSpan(
+                              text: "🥰",
+                              style: TextStyle(fontSize: 50),
+                            ),
+                            TextSpan(
+                              text: "😍",
+                              style: TextStyle(fontSize: 38),
+                            ),
+                          ]),
+                        )
+                      ]),
+                    )
+                  ],
+                )
+              ],
+            )
           ],
         ),
       ),
