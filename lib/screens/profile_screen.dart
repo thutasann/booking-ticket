d062c240587f62d4f31469cec4933455737db3d0
thutasann <thutasann2002@gmail.com>
2023-03-10 09:56:34 +0630

    UI: Profile Banner section

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -19,8 +19,10 @@ class ProfileScreen extends StatelessWidget {
             vertical: AppLayout.getHeight(5),
           ),
           children: [
+            // PROFILE HEADING SECTION
             Gap(AppLayout.getHeight(40)),
             Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Container(
                   height: AppLayout.getHeight(86),
@@ -88,6 +90,100 @@ class ProfileScreen extends StatelessWidget {
                       ]),
                     )
                   ],
+                ),
+                const Spacer(),
+                Column(
+                  children: [
+                    InkWell(
+                      onTap: () {
+                        debugPrint("You are Tapped");
+                      },
+                      child: Text(
+                        "Edit",
+                        style: Styles.textStyle,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+
+            // PROFILE BANNER SECTION
+            Gap(AppLayout.getHeight(8)),
+            Divider(
+              color: Colors.grey.shade300,
+            ),
+            Stack(
+              children: [
+                Container(
+                  height: AppLayout.getHeight(90),
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: primary,
+                    borderRadius: BorderRadius.circular(
+                      AppLayout.getHeight(18),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  right: -45,
+                  top: -40,
+                  child: Container(
+                    padding: EdgeInsets.all(
+                      AppLayout.getHeight(30),
+                    ),
+                    decoration: BoxDecoration(
+                      color: Colors.transparent,
+                      shape: BoxShape.circle,
+                      border: Border.all(
+                        width: 18,
+                        color: const Color(0xFF264CD2),
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: AppLayout.getHeight(25),
+                    vertical: AppLayout.getHeight(20),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      CircleAvatar(
+                        maxRadius: 25,
+                        backgroundColor: Colors.white,
+                        child: Icon(
+                          FluentSystemIcons.ic_fluent_lightbulb_filament_filled,
+                          color: primary,
+                          size: 27,
+                        ),
+                      ),
+                      Gap(AppLayout.getHeight(12)),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "You\'v got a new award",
+                            style: Styles.headLineStyle2.copyWith(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                          Text(
+                            "You have 95 flights in a year",
+                            style: Styles.headLineStyle2.copyWith(
+                              fontWeight: FontWeight.w500,
+                              color: Colors.white.withOpacity(0.9),
+                              fontSize: 16,
+                            ),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
                 )
               ],
             )
